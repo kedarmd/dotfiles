b5b4ffe31f875a4293afd2c838ec7ff3edb737b7
kedarmd <dindekedar@gmail.com>
2024-09-02 22:40:11 +0530

    feat: Sync clipboard between OS & Neovim.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -32,6 +32,14 @@ vim.opt.smartcase = true
 -- Enable termguicolors colors
 vim.opt.termguicolors = true
 
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+    vim.opt.clipboard = 'unnamedplus'
+end)
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
