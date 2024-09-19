bddf6a9a389a465837164b7eeb5b86d2b1ee50f9
kedarmd <dindekedar@gmail.com>
2024-09-19 22:07:37 +0530

    feat: hiding .git folder in neo tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -2,32 +2,33 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-        'MunifTanjim/nui.nvim',
-    },
-    cmd = 'Neotree',
-    keys = {
-        { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-    },
-    opts = {
-        filesystem = {
-            window = {
-                mappings = {
-                    ['\\'] = 'close_window',
-                },
-                position = 'right'
-            },
-            filtered_items = {
-                visible = true,
-                hide_dotfiles = false,
-                hide_gitignored = false,
-            }
-        },
-    },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+				position = "right",
+			},
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				never_show = { ".git" },
+			},
+		},
+	},
 }
 --
 -- return {
