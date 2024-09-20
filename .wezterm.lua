e60c79a9a934063251a622a30b59a2ecf980bad4
kedarmd <dindekedar@gmail.com>
2024-09-20 20:55:44 +0530

    feat: updated wezterm config
    made similar to my default gnome terminal setup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,17 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- Color Scheme
-config.color_scheme = 'nord'
+config.color_scheme = "nord"
 
 -- Font
-config.font = wezterm.font('JetBrainsMono Nerd Font Mono', { weight = 'Medium' })
-config.font_size = 10.5
-config.line_height = 1.15
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" })
+config.font_size = 10.25
+config.line_height = 1.20
+config.cell_width = 1.05
 
 config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
 return config
