3f02f37727012b9d131febe5cec27e8a523aed45
kedarmd <dindekedar@gmail.com>
2024-09-28 11:07:21 +0530

    fix: updated the install.sh script to create correct symlink for neovim

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -sf $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES_DIR/.wezterm.lua ~/.wezterm.lua
 ln -sf $DOTFILES_DIR/.config/starship.toml ~/.config/starship.toml
-ln -sf $DOTFILES_DIR/.config/nvim ~/.config/nvim
+
+# Remove existing nvim directory
+NVIM_DIR=~/.config/nvim
+if [ -d $NVIM_DIR ]; then
+    echo "Found existing nvim config."
+    rm -rf $NVIM_DIR
+    echo "Removed existing nvim config."
+fi
+
+# Create symlink for the nvim directory
+ln -sf $DOTFILES_DIR/.config/nvim $NVIM_DIR
 
 echo "Dotfiles synced successfully."
