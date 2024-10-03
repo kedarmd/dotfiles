8c69ab749595c710537e107064e8615b5cf538e8
kedarmd <dindekedar@gmail.com>
2024-10-03 23:28:10 +0530

    feat: added window padding config for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,10 +11,16 @@ config.font_size = 10.25
 config.line_height = 1.20
 config.cell_width = 1.05
 
+config.hide_mouse_cursor_when_typing = false
+
 -- Window
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-
--- TODO: Add padding related config here
+config.window_padding = {
+	left = 4,
+	right = 4,
+	top = 0,
+	bottom = 0,
+}
 
 return config
