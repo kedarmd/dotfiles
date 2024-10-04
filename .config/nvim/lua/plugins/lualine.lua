07bf54075cdb7ef89423d045da7177000151f89e
kedarmd <dindekedar@gmail.com>
2024-10-05 00:33:07 +0530

    feat: updated lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,19 @@
+local os_logo = function()
+	local os_name = vim.loop.os_uname().sysname
+	if os_name == "Linux" then
+		return "" -- Linux icon
+	elseif os_name == "Darwin" then
+		return "" -- macOS icon
+	elseif os_name == "Windows" then
+		return "" -- Windows icon
+	else
+		return "" -- Fallback icon
+	end
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
 			sections = {
@@ -9,6 +23,9 @@ return {
 						path = 1,
 					},
 				},
+				lualine_x = {},
+				lualine_y = { "filetype" },
+				lualine_z = { os_logo },
 			},
 		})
 	end,
