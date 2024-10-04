a6f9a34dd28aba7972a248ab08674c50fc7d163e
kedarmd <dindekedar@gmail.com>
2024-10-04 23:45:23 +0530

    chore: removed comments

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,18 +1,15 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require("lualine").setup({
-            -- options = {
-            --     theme = 'tokyonight'
-            -- },
-            sections = {
-                lualine_c = {
-                    {
-                        'filename',
-                        path = 1,
-                    }
-                }
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
