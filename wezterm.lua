bc9164b641daaf3672a933678dba58f8465cb874
kedarmd <dindekedar@gmail.com>
2024-11-09 20:46:39 +0530

    chore: commented tmux like keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -20,7 +20,7 @@ config.hide_mouse_cursor_when_typing = false
 -- Window
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = true
+-- config.enable_tab_bar = true
 config.window_padding = {
 	left = 4,
 	right = 4,
@@ -56,85 +56,85 @@ config.colors = {
 }
 
 -- KeyMaps
-config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 } -- timeout_milliseconds defaults to 1000 and can be omitted
-config.keys = {
-	{ key = "0", mods = "CTRL", action = wezterm.action.ResetFontSize },
-	{
-		key = '"',
-		mods = "LEADER|SHIFT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "%",
-		mods = "LEADER|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "z",
-		mods = "LEADER",
-		action = wezterm.action.TogglePaneZoomState,
-	},
-	{
-		key = "x",
-		mods = "LEADER",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "h",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "l",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "k",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "LeftArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		key = "RightArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		key = "UpArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Up", 5 }),
-	},
-	{
-		key = "DownArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "p",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		key = "n",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(1),
-	},
-}
+-- config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 } -- timeout_milliseconds defaults to 1000 and can be omitted
+-- config.keys = {
+-- 	{ key = "0", mods = "CTRL", action = wezterm.action.ResetFontSize },
+-- 	{
+-- 		key = '"',
+-- 		mods = "LEADER|SHIFT",
+-- 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		key = "%",
+-- 		mods = "LEADER|SHIFT",
+-- 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		key = "z",
+-- 		mods = "LEADER",
+-- 		action = wezterm.action.TogglePaneZoomState,
+-- 	},
+-- 	{
+-- 		key = "x",
+-- 		mods = "LEADER",
+-- 		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+-- 	},
+-- 	{
+-- 		key = "h",
+-- 		mods = "CTRL",
+-- 		action = act.ActivatePaneDirection("Left"),
+-- 	},
+-- 	{
+-- 		key = "l",
+-- 		mods = "CTRL",
+-- 		action = act.ActivatePaneDirection("Right"),
+-- 	},
+-- 	{
+-- 		key = "k",
+-- 		mods = "CTRL",
+-- 		action = act.ActivatePaneDirection("Up"),
+-- 	},
+-- 	{
+-- 		key = "j",
+-- 		mods = "CTRL",
+-- 		action = act.ActivatePaneDirection("Down"),
+-- 	},
+-- 	{
+-- 		key = "LeftArrow",
+-- 		mods = "LEADER",
+-- 		action = act.AdjustPaneSize({ "Left", 5 }),
+-- 	},
+-- 	{
+-- 		key = "RightArrow",
+-- 		mods = "LEADER",
+-- 		action = act.AdjustPaneSize({ "Right", 5 }),
+-- 	},
+-- 	{
+-- 		key = "UpArrow",
+-- 		mods = "LEADER",
+-- 		action = act.AdjustPaneSize({ "Up", 5 }),
+-- 	},
+-- 	{
+-- 		key = "DownArrow",
+-- 		mods = "LEADER",
+-- 		action = act.AdjustPaneSize({ "Down", 5 }),
+-- 	},
+-- 	{
+-- 		key = "c",
+-- 		mods = "LEADER",
+-- 		action = act.SpawnTab("CurrentPaneDomain"),
+-- 	},
+-- 	{
+-- 		key = "p",
+-- 		mods = "LEADER",
+-- 		action = act.ActivateTabRelative(-1),
+-- 	},
+-- 	{
+-- 		key = "n",
+-- 		mods = "LEADER",
+-- 		action = act.ActivateTabRelative(1),
+-- 	},
+-- }
 
 config.use_fancy_tab_bar = false
 
