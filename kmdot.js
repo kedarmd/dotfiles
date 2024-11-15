d806a747d3b168681589ee22c224acc5f96f97dc
kedarmd <dindekedar@gmail.com>
2024-11-15 23:45:55 +0530

    feat: updated kmdot to update the theme of tmux as well

--- kmdot.js
+++ kmdot.js
@@ -1,3 +1,4 @@
+import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 
@@ -33,6 +34,54 @@ function updateWeztermTheme(theme) {
     const source = `${weztermThemesDir}/${theme}.lua`;
     setThemeCallback(theme, source, target);
     setThemeCallback(theme, wezterFilePath, wezterFilePath, 'Wezterm');
+}
+
+function updateTmuxTheme(theme) {
+    const startMarker = '# Theme';
+    const endMarker = '# End of theme setup';
+    const tmuxFilePath = path.join(process.env.HOME, 'development', 'dotfiles', 'tmux.conf');
+
+    fs.readFile(tmuxFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error while reading tmux.conf');
+            return;
+        }
+        const startIdx = data.indexOf(startMarker);
+        const endIdx = data.indexOf(endMarker);
+        if (startIdx === -1 || endIdx === -1) {
+            console.error('Could not find theme markers in tmux.conf');
+            return;
+        }
+        const beforeContent = data.substring(0, startIdx);
+        const endContent = data.substring(endIdx + endMarker.length + 1);
+        // console.debug(data.substring(startIdx));
+        const newTheme = readTmuxThemeFile(theme);
+        const newThemeConfig = `${startMarker}
+${newTheme}
+${endMarker}
+`;
+        const newConfig = beforeContent + newThemeConfig + endContent;
+        fs.writeFile(tmuxFilePath, newConfig, 'utf8', (err) => {
+            if (err) {
+                console.error('Error writing tmux.conf file');
+                return;
+            }
+            console.log(`Updated TMUX theme to ${theme}`);
+            exec('tmux source-file ~/.tmux.conf');
+        })
+    })
+}
+
+function readTmuxThemeFile(theme) {
+    const themePath = path.join(process.env.HOME, 'development', 'dotfiles', 'themes', 'tmux', `${theme}.conf`);
+    const data = fs.readFileSync(themePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error while reading tmux theme file: ${theme}.conf`);
+            return;
+        }
+        return data;
+    })
+    return data;
 }
 
 function setThemeCallback(theme, sourcePath, targetPath, config) {
@@ -99,10 +148,11 @@ function main() {
         process.exit(1);
     }
 
+    updateWeztermTheme(theme);
     setNvimTheme(theme);
     setStarshipTheme(theme);
-    updateWeztermTheme(theme);
     updateState(theme);
+    updateTmuxTheme(theme);
     console.log(`All themes changed to ${theme}.`);
 }
 
