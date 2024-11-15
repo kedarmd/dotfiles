da8871bdbf71b9b076c3fb3b73ac58a3539e0a77
kedarmd <dindekedar@gmail.com>
2024-11-15 23:50:32 +0530

    chore: updated onedark nvim theme config

--- themes/nvim/onedark.lua
+++ themes/nvim/onedark.lua
@@ -1,9 +1,10 @@
 return {
 	"navarasu/onedark.nvim",
 	config = function()
-		-- require('onedark').setup {
-		--     style = 'darker'
-		-- }
+		---@diagnostic disable-next-line: undefined-field
+		require("onedark").setup({
+			style = "darker",
+		})
 		vim.cmd.colorscheme("onedark")
 	end,
 }
