782795189ca60dbc674dac4edf95e2bfa7ab26f6
kedarmd <dindekedar@gmail.com>
2024-11-23 23:51:58 +0530

    feat: added function to updated the wezterm tab name based on active pane in the tab

--- wezterm.lua
+++ wezterm.lua
@@ -19,7 +19,7 @@ config.cell_width = 1.05
 config.hide_mouse_cursor_when_typing = false
 
 -- Window
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.window_decorations = "RESIZE"
 -- config.enable_tab_bar = true
 config.window_padding = {
@@ -38,23 +38,23 @@ config.window_padding = {
 -- }
 
 -- Custom Colors that were not covered in color scheme (Mainly tab)
-config.colors = {
-	tab_bar = {
-		background = "#1C212A",
-		active_tab = {
-			bg_color = "#2A313C",
-			fg_color = "#FFFFFF",
-		},
-		inactive_tab = {
-			bg_color = "#14191A",
-			fg_color = "#AAAAAA",
-		},
-		inactive_tab_hover = {
-			bg_color = "#252A2B",
-			fg_color = "#FFFFFF",
-		},
-	},
-}
+-- config.colors = {
+-- 	tab_bar = {
+-- 		background = "#1C212A",
+-- 		active_tab = {
+-- 			bg_color = "#2A313C",
+-- 			fg_color = "#FFFFFF",
+-- 		},
+-- 		inactive_tab = {
+-- 			bg_color = "#14191A",
+-- 			fg_color = "#AAAAAA",
+-- 		},
+-- 		inactive_tab_hover = {
+-- 			bg_color = "#252A2B",
+-- 			fg_color = "#FFFFFF",
+-- 		},
+-- 	},
+-- }
 
 -- KeyMaps
 config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 } -- timeout_milliseconds defaults to 1000 and can be omitted
@@ -143,5 +143,16 @@ config.keys = {
 }
 
 config.use_fancy_tab_bar = false
+-- Function to extract and format tab titles
+wezterm.on("format-tab-title", function(tab)
+	local active_pane = tab.active_pane
+	if active_pane then
+		local cwd = active_pane.current_working_dir
+		local path = cwd.file_path
+		local last_dir = path:match(".*/(.*)")
+		return " " .. tab.tab_index + 1 .. ": " .. last_dir .. " "
+	end
+	return tab.tab_title
+end)
 
 return config
