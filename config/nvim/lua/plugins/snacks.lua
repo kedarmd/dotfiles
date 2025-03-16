9b7bb9017136bdd297de5fe1283a23616867f0a9
kedarmd <dindekedar@gmail.com>
2025-03-16 13:40:17 +0530

    chore: using snacks partialy for some telescope usecases

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -25,7 +25,7 @@ return {
 		explorer = { enabled = true },
 		indent = { enabled = false },
 		input = { enabled = false },
-		picker = { enabled = false },
+		picker = { enabled = true },
 		notifier = { enabled = true },
 		quickfile = { enabled = false },
 		scope = { enabled = false },
@@ -48,5 +48,49 @@ return {
 		"<C-`>",
 		"<cmd>lua Snacks.terminal.toggle()<CR>",
 		{ desc = "Toggle Terminal", noremap = true, silent = true }
+	),
+
+	-- Picker
+	keymap(
+		"n",
+		"<leader>sf",
+		"<cmd>lua Snacks.picker.files()<CR>",
+		{ desc = "[S]earch [F]iles", noremap = true, silent = true }
+	),
+	keymap(
+		"n",
+		"<leader><leader>",
+		"<cmd>lua Snacks.picker.buffers()<CR>",
+		{ desc = "[Search] [Buffers] existing", noremap = true, silent = true }
+	),
+	keymap(
+		"n",
+		"<leader>sw",
+		"<cmd>lua Snacks.picker.grep_word()<CR>",
+		{ desc = "[Search] current [W]ord", noremap = true, silent = true }
+	),
+	keymap(
+		"n",
+		"<leader>sg",
+		"<cmd>lua Snacks.picker.grep()<CR>",
+		{ desc = "[Search] by [G]rep", noremap = true, silent = true }
+	),
+	keymap(
+		"n",
+		"<leader>sd",
+		"<cmd>lua Snacks.picker.diagnostics()<CR>",
+		{ desc = "[Search] [D]iagnostics", noremap = true, silent = true }
+	),
+	keymap(
+		"n",
+		"<leader>sr",
+		"<cmd>lua Snacks.picker.resume()<CR>",
+		{ desc = "[Search] [R]esume", noremap = true, silent = true }
+	),
+	keymap(
+		"n",
+		"<leader>sb",
+		"<cmd>lua Snacks.picker.git_branches()<CR>",
+		{ desc = "[Search] [B]ranches", noremap = true, silent = true }
 	),
 }
